5307fccb55c9d823701dcb37ce8bed54b70ea004
tpo-mtowles <77006920+tpo-mtowles@users.noreply.github.com>
2021-01-26 17:14:31 -0500

    Part 1 and 2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 
+const jobRoleOther = document.getElementsByTagName('option')[6];
+const jobRoleInput = document.getElementById('other-job-role');
+const select = document.querySelector('select');
+
+
+document.addEventListener('input', e => {
+    if (e.target.value === 'other')
+        jobRoleInput.type = 'text';
+});
+
