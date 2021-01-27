321a0c16387a7c67770d28a33730387de3b5d080
tpo-mtowles <77006920+tpo-mtowles@users.noreply.github.com>
2021-01-27 17:29:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const jobRoleInput = document.getElementById('other-job-role');
 const jobRoleSelect = document.querySelector('select');
 const shirtDesignSelect = document.getElementById('design');
 const shirtColorSelect = document.getElementById('color');
+const activitiesFs = document.getElementById('activities');
+const activitiesCb = document.querySelectorAll('[type=checkbox]')
+let totalCost = document.getElementById('activities-cost')
+totalCost.textContent = '';
 
 jobRoleSelect.addEventListener('input', e => {
     if (e.target.value === 'other') {
@@ -15,9 +19,8 @@ jobRoleSelect.addEventListener('input', e => {
 
 
 shirtDesignSelect.addEventListener('input', e => {
-    const colorOptions = shirtColorSelect.querySelectorAll('[data-theme'); //Array of options
-    //e.target = shirtDesignSelect;
-    shirtColorSelect.disabled = false;
+const colorOptions = shirtColorSelect.querySelectorAll('[data-theme'); //Array of options
+shirtColorSelect.disabled = false;
     if (e.target.value === 'heart js' ) {
         for (i = 0; i < colorOptions.length; i++) {
             const theme = colorOptions[i].getAttribute('data-theme');
@@ -41,5 +44,16 @@ shirtDesignSelect.addEventListener('input', e => {
                 }
         }       
     }       
-       
 });
+
+activitiesFs.addEventListener('change', e => {
+    for (i=0; i < activitiesCb.length; i++) {
+        const total = activitiesCb[i].getAttribute('data-cost');
+        let totalValue = parseInt(total.value);
+        if (e.target.checked === true) {
+            totalCost.textContent += `Total: ${totalValue}`;
+        } //else {
+            //totalCost.textContent = totalCost;
+        //}
+    }
+} )
