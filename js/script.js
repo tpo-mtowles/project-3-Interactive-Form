df1310c9c27d1b0798b79b1b520e5ea6dbd9d822
tpo-mtowles <77006920+tpo-mtowles@users.noreply.github.com>
2021-01-27 11:08:12 -0500

    Section 3 progress

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 
-const jobRoleOther = document.getElementsByTagName('option')[6];
+const siteOptions = document.getElementsByTagName('option');
 const jobRoleInput = document.getElementById('other-job-role');
-const select = document.querySelector('select');
+const jobRoleSelect = document.querySelector('select');
+const shirtDesignSelect = document.getElementById('design');
+const shirtColorSelect = document.getElementById('color');
 
-
-select.addEventListener('input', e => {
+jobRoleSelect.addEventListener('input', e => {
     if (e.target.value === 'other') {
         jobRoleInput.type = 'text';
     } else {
@@ -12,3 +13,17 @@ select.addEventListener('input', e => {
     }
 });
 
+
+shirtDesignSelect.addEventListener('input', e => {
+    const shirtJsPuns = siteOptions[16].getAttribute('data-theme');
+    const shirtHeartJs = siteOptions[19].getAttribute('data-theme');
+    shirtColorSelect.disabled = false;
+    if (e.target.getAttribute.value === 'js puns') {
+        for (i = 0; i < shirtHeartJs.length; i++)
+        shirtHeartJs[i].hidden = true;
+    } else {
+        for (i = 0; i < shirtJsPuns.length; i++)
+        shirtJsPuns[i].hidden = true;
+    }
+    
+});
